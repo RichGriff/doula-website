2d9a8a0dec000a309f73cf6c35f6fe7015a8e4a0
Richard Griffiths <rich.griffiths89@gmail.com>
2024-05-29 15:11:27 +0100

    Added to root page

--- app/(admin)/admin/website/page.tsx
+++ app/(admin)/admin/website/page.tsx
@@ -1,18 +1,20 @@
 import { Separator } from '@/components/ui/separator'
 import { contentTypes } from '@/config'
-import React from 'react'
+import React, { Suspense } from 'react'
 import WebsiteMenu from './_components/WebsiteMenu'
 import PageDetail from './_components/PageDetail'
 
 const PagesPage = () => {
   return (
-    <div className='flex justify-start items-start w-full'>
-      <WebsiteMenu />
-      
-      <div className='w-3/4'>
-        <PageDetail />
+    <Suspense>
+      <div className='flex justify-start items-start w-full'>
+        <WebsiteMenu />
+        
+        <div className='w-3/4'>
+          <PageDetail />
+        </div>
       </div>
-    </div>
+    </Suspense>
   )
 }
 
