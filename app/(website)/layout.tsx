8b62b340858b4d3cbee3eadce43bd7ce5e6c4a9c
Richard Griffiths <rich.griffiths89@gmail.com>
2024-05-29 09:29:56 +0100

    Initial Set Up

---
+++ app/(website)/layout.tsx
@@ -0,0 +1,12 @@
+import { ReactNode } from "react"
+
+
+const WebsiteLayout = ({ children } : { children: ReactNode }) => {
+  return (
+    <>
+      {children}
+    </>
+  )
+}
+
+export default WebsiteLayout
---
+++ app/(website)/page.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <main>
+      <h2>Hello Doula!</h2>
+    </main>
+  );
+}
